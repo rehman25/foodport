e4aa2f640e0139c9518fcb9770c86f64ddb2961d
ghayas ali <team@logochemist.com>
2022-05-24 23:52:58 +0500

    Ghayas

--- pages/app/store.js
+++ pages/app/store.js
@@ -2,11 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
  import userReducer from "../features/UserSlice"
  import ResReducer from "../features/ResSlice"
  import BasketReducer from "../features/BasketSlice"
-const configureStore=({
+export default configureStore({
     reducer:{
          resturant:ResReducer,
         user:userReducer,
          basket:BasketReducer
     },
 });
-export default configureStore;
