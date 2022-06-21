e5e763aafa7fb1b974cc01b75e5ab181c1d5b3dc
unknown <team@logochemist.com>
2022-06-21 23:20:41 +0500

    Ghayas

--- pages/api/connectedaccount.js
+++ pages/api/connectedaccount.js
@@ -34,7 +34,7 @@ export default async(req, res)=> {
       );
       const accountLink = await stripe.accountLinks.create({
         account: account.id,
-        success_url:`${process.env.HOST}/ResturentOwner`,
+        success_url:`${process.env.HOST}/success`,
         failure_url:`${process.env.HOST}/Main_login`,
         type: 'account_onboarding',
       });
